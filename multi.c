38e88a030e13124d503ef23b8473079591f1da00
pampanaeswari30-pixel <pampanaeswari30@gmail.com>
2025-12-02 12:51:44 +0530

    Add files via upload

---
+++ multi.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+   int a,b,c;
+   scanf("%d%d,&a,&b");	
+	c=a*b;
+	printf("product of the given numbers is:%d",c);
+return 0;
+}
+	
+	
+	
+	
+	
+
+
+
+
+
+
+
+
+
+
+
